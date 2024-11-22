378839abaff0a9d0787014ccf61f193c99ae7cbf
tftcode <tftcode@gmail.com>
2024-11-22 16:25:08 +0000

    vs-code-extensions.json added

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Symlink VS Code settings
-ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-ln -sf ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+# ln -sf /home/user/dotfiles/vscode-user-settings.json /home/user/dotfiles/.vscode/settings.json
+# ln -sf /home/user/dotfiles/vscode-user-keybinding.json /home/user/dotfiles/.vscode/keybindings.json
 
 # Add other symlinks as needed
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+#ln -sf ~/dotfiles/.bashrc ~/.bashrc
+#ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-echo "Dotfiles setup complete!"
+# echo "Dotfiles setup complete!"
+
+# Download the settings.json file from GitHub
+curl -o /home/user/dotfiles/.vscode/settings.json https://raw.githubusercontent.com/tekacode/dotfiles/refs/heads/master/vscode-user-settings.json
+# Download the keybindings.json file from GitHub
+curl -o /home/user/dotfiles/.vscode/keybindings.json https://raw.githubusercontent.com/tekacode/dotfiles/refs/heads/master/vscode-user-keybinding.json
+echo "VS Code settings have been fetched from the GitHub repository."
